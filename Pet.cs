9bf22bbe3e5e50e49a84e412cefe89ed75a6d07d
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-09-16 11:08:06 +0200

    Add project files.

---
+++ Pet.cs
@@ -0,0 +1,11 @@
+﻿namespace CALINQ24091602
+{
+    internal class Pet
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age => (int)(DateTime.Now - BirthDate).TotalDays / 365;
+        public bool Sex { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using CALINQ24091602;
+
+#region list of pets
+List<Pet> pets = [
+    new Pet() //01
+    {
+        Name = "Mr. Wick",
+        Species = "hamster",
+        BirthDate = new(2022, 05, 05),
+        Sex = true,
+    },
+    new Pet() //02
+    {
+        Name = "Ferenc",
+        Species = "cat",
+        BirthDate = new(2018, 01, 02),
+        Sex = false,
+    },
+    new Pet() //03
+    {
+        Name = "Maci",
+        Species = "dog",
+        BirthDate = new(2019, 11, 10),
+        Sex = true,
+    },
+    new Pet() //04
+    {
+        Name = "Macska",
+        Species = "cat",
+        BirthDate = new(2024, 02, 10),
+        Sex = false,
+    },
+    new Pet() //05
+    {
+        Name = "Hal",
+        Species = "fish",
+        BirthDate = new(2005, 04, 15),
+        Sex = true,
+    },
+    new Pet() //06
+    {
+        Name = "Sziszi",
+        Species = "dog",
+        BirthDate = new(2022, 09, 20),
+        Sex = false,
+    },
+    new Pet() //07
+    {
+        Name = "Csirke",
+        Species = "chicken",
+        BirthDate = new(2020, 08, 07),
+        Sex = false,
+    },
+];
+#endregion
+
